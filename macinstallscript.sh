b07d7e6e20003d9a7fefd50e927dce2ff801141c
DylanRitchings1998@gmail.com <DylanRitchings1998@gmail.com>
2022-07-27 18:07:47 +0100

    added

--- macinstallscript.sh
+++ macinstallscript.sh
@@ -2,23 +2,40 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
-function brewFor {
+function brewAppFor {
     for app in $1;
     do
         brew install --cask "$app"
     done
 }
 
+function brewTermFor {
+    for app in $1;
+    do
+        brew install "$app"
+    done
+}
+
 
 #Global
+
+globalTerm (
+    git
+  )
 
 globalApp=(
     brave-browser
     emacs
 )
 
-brew install --cask brave-browser
+mkdir ~/.doom.d
 
+cd ~/.doom.d
+
+git clone https://github.com/DylanRitchings/DoomConfig.git
+
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+~/.emacs.d/bin/doom install
 
 #Work
 
@@ -26,9 +43,11 @@ workApp=(
 )
 
 
+
 #home
 
 homeApp=(
     ableton-live-standard
     steam
+    
 )
